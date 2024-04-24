d3d26c83a86e48f96fd7282a74dd9f14c2807306
Дмитрий <165063315+MrJonson75@users.noreply.github.com>
2024-04-24 18:04:00 +0300

    Урок "Модули и пакеты"

---
+++ lesson.py
@@ -0,0 +1,7 @@
+test_param = "Hello, world"
+
+def test_function1():
+    print(f'Первая функция выводит {test_param}')
+
+def test_function2():
+    print(f'Вторая функция выводит {test_param}')
---
+++ main.py
@@ -0,0 +1,4 @@
+import lesson as t_f
+
+t_f.test_function1()
+t_f.test_function2()
