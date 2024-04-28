d3f5558e81917992fdcbb3a734abf96388251edf
Дмитрий <165063315+MrJonson75@users.noreply.github.com>
2024-04-28 17:09:08 +0300

    Пространство имен и области видимости.

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-import lesson as t_f
+#Пространство имен и области видимости.
+def test_function(): #Первая функция
+    print("Привет ")
+    def inner_function(): #Функция которую нужно вызвать из первой функции.
+        print("Я в области видимости функции test_function")
+    inner_function()
+test_function()
+# inner_function() напрямую вызвать функцию нельзя, ниже код как можно
+def test_function1(flag=0):
+    def inner_function1():
+        print("Я в области видимости функции test_function1")
 
-t_f.test_function1()
-t_f.test_function2()
+    if flag:
+        print("Вызов из вложенной функции")
+    else:
+        inner_function1()
+test_function1()
+test_function1(1)
