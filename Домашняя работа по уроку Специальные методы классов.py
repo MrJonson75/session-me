9aed359c4f9f8c9210deac34bea24bfc55cd9271
Дмитрий <165063315+MrJonson75@users.noreply.github.com>
2024-05-12 20:38:23 +0300

    Update Домашняя работа по уроку Специальные методы классов.py

--- Домашняя работа по уроку Специальные методы классов.py
+++ Домашняя работа по уроку Специальные методы классов.py
@@ -1,13 +1,14 @@
 class House:
-    def __init__(self, floors):
-        self.numberOfFloors = floors
+    def __init__(self):
+        self.numberOfFloors = 0
 
-    def setNewNumberOfFloors(self):
+    def setNewNumberOfFloors(self , floors):
+        self.numberOfFloors = floors
         print(f"В этом доме: {self.numberOfFloors} этажей")
 
-    pass
 
-my_hause = [House(floors=13), House(floors=25), House(floors=65)]
-my_home= House(my_hause)
+
+my_hause = [13, 25, 65]
+my_home = House()
 for home_obj in my_hause:
-    home_obj.setNewNumberOfFloors()
+    House.setNewNumberOfFloors(self=my_home, floors=home_obj)
