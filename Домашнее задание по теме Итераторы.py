0fb439ad1d1491045ffa374ee50f9c8b5572242d
Дмитрий <165063315+MrJonson75@users.noreply.github.com>
2024-06-09 12:34:23 +0300

    Update Домашнее задание по теме Итераторы.py
    Доработано

--- Домашнее задание по теме Итераторы.py
+++ Домашнее задание по теме Итераторы.py
@@ -3,27 +3,24 @@ class EvenNumbers:
     def __init__(self, start=0, end=1):
         self.start = start
         self.end = end
-        self.step = 0
 
     def __iter__(self):
-        self.step = self.start - 1
-        return self
+        if self.start % 2 == 0:
+            self.start -= 2
+            return self
+        else:
+            self.start -= 1
+            return self
 
     def __next__(self):
-        self.step +=1
-        if self.step > self.start:
-            if self.step >= self.end:
-                raise StopIteration()
-        if self.step % 2 == 0:
-            return self.step
+        self.start += 2
+        if self.start >= self.end:
+            raise StopIteration()
         else:
-            self.step +=1
-            return self.step
-
-
-
+            return self.start
 
 
 en = EvenNumbers(10, 25)
 for i in en:
     print(i)
+
