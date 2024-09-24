905cbe1effc8ef68b60a7ada2255e2a058b8e3cd
MrJonson <flashh@list.ru>
2024-09-24 21:05:34 +0300

    Домашнее задание по теме "Сайт администрирования"

--- mysite/task1/admin.py
+++ mysite/task1/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import *
+
+# admin.site.register(Buyer)
+# admin.site.register(Game)
 
 # Register your models here.
+
+@admin.register(Game)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size', 'age_limited',)
+    fields = [('title', 'cost', 'size', 'age_limited'), 'description', 'buyer',]
+    list_filter = ('title', 'age_limited', 'buyer',)
+    search_fields = ('title',)
+
+
+@admin.register(Buyer)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'balance',)
+    fields = [('name', 'age', 'balance'), 'password']
+    search_fields = ('name',)
